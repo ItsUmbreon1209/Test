f3b72ba8b0a67ca58c2742eee9552878558f0531
ItsUmbreon1209 <73486721+ItsUmbreon1209@users.noreply.github.com>
2020-10-26 19:54:25 -0500

    Add files via upload

---
+++ Test.ino
@@ -0,0 +1,93 @@
+#include <Servo.h>
+Servo myservo;
+
+void setup() {
+  myservo.attach(9);}
+
+int pos = 0;
+
+void loop() {
+for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}
+for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 0; pos <= 360; pos += 1) { 
+    myservo.write(86);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}
+for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}for (pos = 360; pos >= 0; pos -= 1) {
+    myservo.write(94);              
+    delay(2);}
+for (pos = 360; pos >= 0; pos -= 1) { 
+    myservo.write(93);              
+    delay(5);}
+}
